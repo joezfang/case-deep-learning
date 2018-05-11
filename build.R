760256d1049cd6797aadafa9333dade106bbaf16
kevinykuo <kevinykuo@gmail.com>
2018-05-11 13:52:33 -0400

    some hygge

--- build.R
+++ build.R
@@ -1,2 +1,3 @@
-rmarkdown::render("presentation.Rmd", output_file = "public/index.html")
+rmarkdown::render("presentation.Rmd",
+                  output_file = rprojroot::find_rstudio_root_file("public/index.html"))
 rmarkdown::render_supporting_files("static/figs", "public")
